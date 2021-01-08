a3157dc64f0ae181d78a01b83d9f52f741a46cac
Florian Kretlow <floriankretlow@gmx.de>
2021-01-08 16:30:56 +0100

    clean up

--- src/0004_median.cpp
+++ src/0004_median.cpp
@@ -2,106 +2,64 @@
 #include <iostream>
 #include <vector>
 
-double find_median(const std::vector<int>& nums)
+double find_median(const std::vector<int>& V)
 {
-    std::vector<int>::size_type i = nums.size() / 2;
-    if (nums.size() % 2 == 0) {
-        return (nums[i-1] + nums[i]) / 2.0;
-    } else {
-        return nums[i];
-    }
+    std::vector<int>::size_type m = V.size() / 2;
+    if (V.size() & 1) return V[m];
+    else              return (V[m-1] + V[m]) / 2.0;
 }
 
-double find_median(const std::vector<int>& nums1, const std::vector<int>& nums2)
+double find_median_of_successive_arrays(const std::vector<int>& low,
+                                        const std::vector<int>& high)
 {
-    if      (nums1.size() == 0) return find_median(nums2);
-    else if (nums2.size() == 0) return find_median(nums1);
+    /* Assume both arrays are sorted and low.back() <= high.front(). */
+    std::vector<int>::size_type n = low.size() + high.size();
+    std::vector<int>::size_type i = n / 2;
+    if (n & 1) return i < low.size() ? low.at(i)   : high.at(i - low.size());
+    else return  ( (i-1 < low.size() ? low.at(i-1) : high.at(i-1 - low.size()))
+                 + (i   < low.size() ? low.at(i)   : high.at(i - low.size())))
+                 / 2.0;
+}
+
+double find_median(const std::vector<int>& V1, const std::vector<int>& V2)
+{
+    /* Peel off edge cases. */
+    if      (V1.size() == 0)          return find_median(V2);
+    else if (V2.size() == 0)          return find_median(V1);
+    else if (V1.back() <= V2.front()) return find_median_of_successive_arrays(V1, V2);
+    else if (V2.back() <= V1.front()) return find_median_of_successive_arrays(V2, V1);
+
+    /* Now we know that the arrays are intertwined, which eliminates a thicket of awkward edge
+     * cases. Perform a binary search on the smaller array, A, to find two indices i, j such that
+     *     (1) i + j = |A U B| / 2
+     *     (2) A[i] >= B[j-1] and B[j] >= A[i-1]. */
 
     const std::vector<int> *A, *B;
-    if (nums1.size() < nums2.size()) { A = &nums1; B = &nums2; }
-    else                             { A = &nums2; B = &nums1; }
+    if (V1.size() < V2.size()) { A = &V1; B = &V2; }
+    else                       { A = &V2; B = &V1; }
     assert(A->size() <= B->size());
 
-    std::vector<int>::size_type i, j, hi, lo, m;
+    std::vector<int>::size_type i, j, lo, hi, m;
     m = (A->size() + B->size()) / 2;
-    i = A->size() / 2;
-    j = m - i;
     lo = 0;
     hi = A->size();
 
     for ( ;; ) {
-        std::cout << "i=" << i << ", j=" << j << std::endl;
-        if (i == A->size())                         break;
-        else if (A->at(i) < B->at(j-1))             lo = i + 1;
-        else if (i > 0 && B->at(j) < A->at(i-1))    hi = i;
-        else                                        break;
-        i = lo + (hi - lo) / 2;
+        i = (lo + hi) / 2;
         j = m - i;
-        if (i == 0)                                 break;
+        if               (A->at(i) < B->at(j-1)) lo = i + 1;
+        else if (i > 0 && B->at(j) < A->at(i-1)) hi = i;
+        else                                     break;
     }
 
-    std::cout << "final i=" << i << ", j=" << j << std::endl;
-
-    if ((A->size() + B->size()) % 2 == 0) {
-        std::cout << "even" << std::flush;
-        double med_upper, med_lower;
-        if (i == 0) {
-            std::cout << ", i=0" << std::flush;
-            if (A->size() == B->size()) {
-                std::cout << ", equal size" << std::endl;
-                med_upper = A->front();
-                med_lower = B->back();
-            } else {
-                std::cout << ", unequal size" << std::endl;
-                med_upper = std::min(A->at(i), B->at(j));
-                med_lower = B->at(j-1);
-            }
-        } else if (i == A->size()) {
-            std::cout << ", i=|A|" << std::flush;
-            if (A->size() == B->size()) {
-                std::cout << ", equal size" << std::endl;
-                med_upper = B->front();
-                med_lower = A->back();
-            } else {
-                std::cout << ", unequal size" << std::endl;
-                med_upper = B->at(j);
-                med_lower = std::max(A->back(), B->at(j-1));
-            }
-        } else {
-            std::cout << ", 0 < i < |A|" << std::endl;
-            med_upper = std::min(A->at(i), B->at(j));
-            med_lower = std::max(A->at(i-1), B->at(j-1));
-        }
-        return (med_upper + med_lower) / 2;
-    } else {
-        std::cout << "odd" << std::endl;
-        if (i == A->size()) {
-            return B->at(j);
-        } else {
-            return std::min(A->at(i), B->at(j));
-        }
-    }
-}
-
-double find_median_brute_force(std::vector<int>& A, std::vector<int>& B)
-{
-    std::vector<int>::size_type m = (A.size() + B.size()) / 2;
-    auto itA = A.cbegin();
-    auto itB = B.cbegin();
-    int cur;
-    for (std::vector<int>::size_type i = 0; i <= m; ++i) {
-        if (*itA < *itB) cur = *itA++;
-        else             cur = *itB++;
-    }
-    return cur;
+    if ((A->size() + B->size()) & 1) return std::min(A->at(i), B->at(j));
+    else return (std::max(A->at(i-1), B->at(j-1)) + std::min(A->at(i), B->at(j))) / 2.0;
 }
 
 int main()
 {
-    std::vector<int> A { 1,3 };
-    std::vector<int> B { 2 };
-
-    double m = find_median(A, B);
-    std::cout << m << std::endl;
+    std::vector<int> A { 2,4 };
+    std::vector<int> B { 1,3 };
+    std::cout << find_median(A, B) << std::endl;
     return 0;
 }
